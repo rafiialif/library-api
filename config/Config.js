dcde6b65409d8617c9f99fe7449d988907a4f781
rafiialif <rafiialif27@gmail.com>
2020-02-17 20:22:46 +0700

    init

--- config/Config.js
+++ config/Config.js
@@ -1,11 +1,5 @@
 
 module.exports = {
-  // TODO: Tambahkan konfigurasi untuk
-  // DB sebagai host mongoDB instance yang melihat pada environment variable `HOST`,
-  //    jika environment variables tidak tersedia, makan nilai DB menjadi `localhost`
-  // DB:
-
-  // APP_PORT sebagai PORT untuk menjalankan project express dengan environment variable `HOST`
-  //          jika environment variable tidak tersedia, maka nilai APP_PORT menjadi 80
-  // APP_PORT: 
+  DB: process.env.HOST ? process.nextTick.HOST : 'mongodb+srv://rafiialif:Alif27061998@sandbox-workshop-wbggu.mongodb.net/learnit-react?retryWrites=true&w=majority',
+  APP_PORT: process.env.APP_PORT ? process.env.APP_PORT : 80,
 };
--- repos/LibraryRepo.js
+++ repos/LibraryRepo.js
@@ -9,34 +9,38 @@ class LibraryRepo {
   // TODO: Buat fungsi dengan nama `tambahBuku` untuk menambah buku baru pada perpustakaan
   // Parameter infoBuku merupakan sebuah objek buku dengan key judulBuku, pengarangBuku, genreBuku
   async tambahBuku(infoBuku) {
-    // tambahkan disini
+    const newBuku = {judulBuku:infoBuku.judulBuku, pengarangBuku:infoBuku.pengarangBuku, genreBuku:infoBuku.genreBuku, isDipinjam:false};
+        const buku = new this.model(newBuku);
+        return await buku.save();
   }
 
   // TODO: Buat fungsi dengan nama `listBuku` untuk mendapatkan semua data buku yg tersedia
   async listBuku() {
-    // tambahkan disini
+      return await this.model.find();
   }
 
   // TODO: Buat fungsi dengan nama `cariBuku` untuk mencari buku berdasarkan ID buku
   async cariBuku(id) {
-    // tambahkan disini
+      return await this.model.findByID(id);
   }
 
   // TODO: Buat fungsi `hapusBuku` untuk dapat menghapus buku dengan ID
   async hapusBuku(id) {
-    // tambahkan disini
+      return await this.model.findByIDAndDelete(id);
   }
 
   // TODO: Buat fungsi dengan nama `rubahInfoBuku` untuk merubah informasi buku dengan ID
   async rubahInfoBuku(id, infoBukuBaru) {
-    // tambahkan disini
+      const query = {_id: id};
+      return await this.model.findOneAndUpdate(query, {$set: infoBukuBaru});
   }
 
   // Fungsi dengan nama `rubahStatusPeminjaman` untuk merubah informasi buku dengan ID
   async rubahStatusPeminjaman(id) {
     const buku = await this.model.findById(id)
-    const query = { _id: id };
-    return await this.model.findOneAndUpdate(query, { $set: { isDipinjam: !(buku.isDipinjam) } });
+    console.log(buku)
+    const query = {_id: id };
+    return await this.model.findOneAndUpdate(query, { $set: {isDipinjam: !(buku.isDipinjam) } });
   }
 }
 
